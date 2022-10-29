bda7709e2cc15e0248680c540ea2b4516a8972ea
StoneTanya <kameneva.tania@mail.ru>
2022-10-29 18:43:45 +0300

    add useState and styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,25 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import styles from './cards.module.scss';
 
 
+const style = {
+    tarifSelected: {
+        backgroundColor: "rgba(238, 130, 238, 0.5)",
+        boxShadow: "0 0 10px #EE82EE",
+        color: "#111",
+    }
+}
+
 export default function CardTarif(props) {
-    const {tarif, toggleSelectTarif} = props
-    useEffect(() => {}, [tarif]) 
+    const {tarif, selectTarif} = props
+    useEffect(() => {}, [tarif]);
+
     return (
         <>
             <div className='col-3 mt-3 mb-3'>
-                <Card style={tarif.selected ? {backgroundColor: "red"} : {}}>
+                <Card style={tarif.selected ? style.tarifSelected : {}}>
                     <Card.Header className={styles.card__name}>{tarif.name}</Card.Header>
                     <ListGroup variant="flush">
-                        <ListGroup.Item className={styles.card__price} onClick={() => toggleSelectTarif(tarif.id)}>руб <span className={styles.price} >{tarif.price} </span> /мес </ListGroup.Item>
+                        <ListGroup.Item className={styles.card__price} onClick={() => selectTarif(tarif.id)}>руб <span className={styles.price} >{tarif.price} </span> /мес </ListGroup.Item>
                         <ListGroup.Item className={styles.card__speed}> до <span>{tarif.speed}</span> Мбит/сек  </ListGroup.Item>
                         <ListGroup.Item className={styles.card__footer}>Объем включенного трафика не ограничен</ListGroup.Item>
                     </ListGroup>
--- src/components/cardsComponent.js
+++ src/components/cardsComponent.js
@@ -1,28 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import tarifsData from "./cardsData";
 import CardElement from "./Card";
 
 function CardsBlock() {
-    const [tarif, setTarif] = useState(tarifsData);
-    const toggleSelectTarif = (tarifID) => {
-        const changedTarifs = tarifsData.map(tarif => {
-            if (tarif.id === tarifID) {
-                return {...tarif, selected: !tarif.selected}
-            }
-            return tarif
+    const [tarifs, setSelected] = useState(tarifsData);
+    
+    const selectTarif = (id) => {           //функция выбора тарифа, запускаемая по клику 
+        const selectedTarif = tarifs.map(tarif => {
+            if (tarif.id === id) {
+            return {...tarif, selected: true}
+        }
+        return tarif
         })
-        setTarif(changedTarifs)
+        setSelected(selectedTarif)
     }
-
-
+    console.log(tarifs);
     return (
-    tarifsData.map(tarif => 
-    <CardElement
-    key={tarif.id} 
-    tarif={tarif}
-    toggleSelectTarif={toggleSelectTarif}
-/>)
-        
+        tarifs.map(tarif =>
+            <CardElement 
+                key={tarif.id}
+                tarif={tarif}
+                selectTarif={selectTarif}
+            />)
+
     )
 }
 
